082a35afc38bc2c8b8e62aa88f64522b759cf24a
Lucas Oliveira <olivmath@protonmail.com>
2024-02-06 00:23:26 -0300

    add: new types to improve readability

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,8 @@ pub mod merkle_proof_check;
 pub mod merkle_root;
 pub mod merkletree;
 pub mod utils;
+
+pub type Proof = Vec<Node>;
+pub type Hash = [u8; 32];
+pub type Leaf = [u8; 32];
+pub type Root = [u8; 32];
